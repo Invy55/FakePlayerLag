3b5a6e1b91e9eb399d3e3b16cb3cbddcbc9a9757
Invy55 <38048104+Invy55@users.noreply.github.com>
2020-03-31 14:16:43 +0200

    Very big fix
    * Fixed another reload issue
    * Fixed a bug with names and classes
    * Fixed the saving of the current admin victim

--- src/Invy55/FakePlayerLag/Main.php
+++ src/Invy55/FakePlayerLag/Main.php
@@ -36,13 +36,20 @@ class Main extends PluginBase implements Listener{
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->stickName = '§cFake§bPlayer§aLag §6Stick';
+        $this->playerL = new \stdClass();
+        foreach($this->getServer()->getOnlinePlayers() as $player){
+            self::setAsDefault($player->getName());
+        }
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
 		$player = $sender->getName();
 		switch($command->getName()){
             case "fpl":
-                self::getStick($sender);
+                if(isset($args[0]) and in_array($args[0], ['reset', 'r', '0', 'remove', 'delete'])){
+                    self::setAsDefault($player, false);
+                    $sender->sendMessage('§aSuccessfully resetted your Lag Parameters');
+                }else self::getStick($sender);
 			default:
 				return false;
         }
@@ -55,7 +62,7 @@ class Main extends PluginBase implements Listener{
 
     public function onQuit(PlayerQuitEvent $event){
         $player = $event->getPlayer()->getName();
-        unset($this->$player);
+        unset($this->playerL->$player);
     }
 
     public function onEntityDamage(EntityDamageByEntityEvent $event) {
@@ -72,56 +79,58 @@ class Main extends PluginBase implements Listener{
 
     public function onPlayerMove(PlayerMoveEvent $event) {
         $player = $event->getPlayer()->getName();
-        if(!isset($this->$player)) self::setAsDefault($player);
-        if(isset($this->$player->latestMove)) $passed = microtime(true) - $this->$player->latestMove; else{ $this->$player->latestMove = microtime(true); $passed = 1;}
-        if($this->$player->moveLag > 0 and ($passed >= 1.00/($this->$player->moveLag/10)+mt_rand(10,30)/100 or $this->$player->moveLag == 100)){
-            if(self::randomizer($this->$player->moveLag)){
+        if(!isset($this->playerL->$player)) self::setAsDefault($player);
+        if(isset($this->playerL->$player->latestMove)) $passed = microtime(true) - $this->playerL->$player->latestMove; else{ $this->playerL->$player->latestMove = microtime(true); $passed = 1;}
+        if($this->playerL->$player->moveLag > 0 and ($passed >= 1.00/($this->playerL->$player->moveLag/10)+mt_rand(10,30)/100 or $this->playerL->$player->moveLag == 100)){
+            if(self::randomizer($this->playerL->$player->moveLag)){
                 $event->setCancelled(true);
-                $this->$player->latestMove = microtime(true);
+                $this->playerL->$player->latestMove = microtime(true);
             }
         }
     }
 
     public function onBlockPlace(BlockPlaceEvent $event) {
         $player = $event->getPlayer()->getName();
-        if(!isset($this->$player)) self::setAsDefault($player);
-        if(isset($this->$player->latestPlace)) $passed = microtime(true) - $this->$player->latestPlace; else{ $this->$player->latestPlace = microtime(true); $passed = 1;}
-        if($this->$player->placeLag > 0 and ($passed >= 1.00/($this->$player->placeLag/10) or $this->$player->placeLag == 100)){
-            if(self::randomizer($this->$player->placeLag)){
+        if(!isset($this->playerL->$player)) self::setAsDefault($player);
+        if(isset($this->playerL->$player->latestPlace)) $passed = microtime(true) - $this->playerL->$player->latestPlace; else{ $this->playerL->$player->latestPlace = microtime(true); $passed = 1;}
+        if($this->playerL->$player->placeLag > 0 and ($passed >= 1.00/($this->playerL->$player->placeLag/10) or $this->playerL->$player->placeLag == 100)){
+            if(self::randomizer($this->playerL->$player->placeLag)){
                 $event->setCancelled(true);
-                $this->$player->latestPlace = microtime(true);
+                $this->playerL->$player->latestPlace = microtime(true);
             }
         }
     }
 
     public function onBlockBreak(BlockBreakEvent $event) {
         $player = $event->getPlayer()->getName();
-        if(!isset($this->$player)) self::setAsDefault($player);
-        if(isset($this->$player->latestBreak)) $passed = microtime(true) - $this->$player->latestBreak; else{ $this->$player->latestBreak = microtime(true); $passed = 1;}
-        if($this->$player->breakLag > 0 and ($passed >= 1.00/($this->$player->breakLag/10) or $this->$player->breakLag == 100)){
-            if(self::randomizer($this->$player->breakLag)){
+        if(!isset($this->playerL->$player)) self::setAsDefault($player);
+        if(isset($this->playerL->$player->latestBreak)) $passed = microtime(true) - $this->playerL->$player->latestBreak; else{ $this->playerL->$player->latestBreak = microtime(true); $passed = 1;}
+        if($this->playerL->$player->breakLag > 0 and ($passed >= 1.00/($this->playerL->$player->breakLag/10) or $this->playerL->$player->breakLag == 100)){
+            if(self::randomizer($this->playerL->$player->breakLag)){
                 $event->setCancelled(true);
-                $this->$player->latestBreak = microtime(true);
+                $this->playerL->$player->latestBreak = microtime(true);
             }
         }
     }
 
     public function openMen(Player $admin, Player $victim){
         $victim = $victim->getName();
-        $this->$admin->currentVictim = $victim;
+        $adminN = $admin->getName();
+        $this->playerL->$adminN->currentVictim = $victim;
         $form = new CustomForm(function (Player $player, $data = null) {
             if ($data === null) return true;
-            $victim = $this->$player->currentVictim->getName();
-            unset($this->$player->currentVictim);
-            $player->sendMessage('§aSuccessfully set Lag Parameters of '.$victim->getName());
-            $this->$victim->moveLag = intval($data[0]);
-            $this->$victim->placeLag = intval($data[1]);
-            $this->$victim->breakLag = intval($data[2]);
+            $adminN = $player->getName();
+            $victim = $this->playerL->$adminN->currentVictim;
+            unset($this->playerL->$adminN->currentVictim);
+            $player->sendMessage('§aSuccessfully set Lag Parameters of '.$victim);
+            $this->playerL->$victim->moveLag = intval($data[0]);
+            $this->playerL->$victim->placeLag = intval($data[1]);
+            $this->playerL->$victim->breakLag = intval($data[2]);
         });
         $form->setTitle('Editing lag of '.$victim);
-        $defaultMoveLag = $this->$victim->moveLag;
-        $defaultPlaceLag = $this->$victim->placeLag;
-        $defaultBreakLag = $this->$victim->breakLag;
+        $defaultMoveLag = $this->playerL->$victim->moveLag;
+        $defaultPlaceLag = $this->playerL->$victim->placeLag;
+        $defaultBreakLag = $this->playerL->$victim->breakLag;
         $form->addSlider('Player Move Lag', 0, 100, -1, $defaultMoveLag);
         $form->addSlider('Block Place Lag', 0, 100, -1, $defaultPlaceLag);
         $form->addSlider('Block Break Lag', 0, 100, -1, $defaultBreakLag);
@@ -138,11 +147,11 @@ class Main extends PluginBase implements Listener{
         if(mt_rand(0, 100) <= $number) return true; else return false;
     }
 
-    public function setAsDefault(String $player){
-        $this->$player = new \stdClass();
+    public function setAsDefault(String $player, bool $set = true){
+        if($set) $this->playerL->$player = new \stdClass();
         //All attributes set here
-        $this->$player->moveLag = 0;
-        $this->$player->placeLag = 0;
-        $this->$player->breakLag = 0;
+        $this->playerL->$player->moveLag = 0;
+        $this->playerL->$player->placeLag = 0;
+        $this->playerL->$player->breakLag = 0;
     }
 }
